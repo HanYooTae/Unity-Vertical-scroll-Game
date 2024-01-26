ec356dc479775030ca76ba2ee25a827e656e5f3b
HanYooTae <han931120@gmail.com>
2024-01-26 18:18:02 +0900

    Make Player & Player Movement

--- Assets/Vertical 2D Shooting BE4/Sprites/Scripts/Player.cs
+++ Assets/Vertical 2D Shooting BE4/Sprites/Scripts/Player.cs
@@ -5,12 +5,86 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchRight;
+    public bool isTouchLeft;
+
+    Animator anim;
+
+    void Awake()
+    {
+        // 변수 초기화
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
+        if ((isTouchRight && h == 1) || (isTouchLeft && h == -1))
+            h = 0;
         float v = Input.GetAxisRaw("Vertical");
-        Vector3 curPos = transform.position;
+        if ((isTouchTop && v == 1) || (isTouchBottom && v == -1))
+            v = 0;
+
+            Vector3 curPos = transform.position;
         Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+        transform.position = curPos + nextPos;
+
+        if (Input.GetButtonDown("Horizontal") ||
+            Input.GetButtonUp("Horizontal"))
+        {
+            anim.SetInteger("Input", (int)h);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch(collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = true;
+                    break;
+
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+
+                case "Right":
+                    isTouchRight = true;
+                    break;
+
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+
+                case "Right":
+                    isTouchRight = false;
+                    break;
+
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+            }
+        }
     }
 }
